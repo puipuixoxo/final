ce5e4d40a85b6a1b21284c3235ab6ff9a4137a35
wei <e09358399@gmail.com>
2023-06-07 19:41:11 +0800

    add fruit & music

---
+++ final411004134/final411004134/Controllers/UserController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace final411004134.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Music(string name,float? number)
+        {
+            var result = "";
+            if (number.HasValue){
+                 if (number == 1){
+                     result = "do";
+                    }
+                 if (number == 2){
+                        result = "re";
+                    }
+                 if (number == 3){
+                        result = "mi";
+                    }
+                 if (number == 4){
+                        result = "fa";
+                    }
+                 if (number == 5){
+                        result = "so";
+                    }
+                 if (number == 6)
+                    {
+                        result = "la";
+                    }
+                 if (number == 7)
+                    {
+                        result = "xi";
+                    }
+                 if(number !=1 && number != 2 && number != 3 && number != 4 && number != 5 && number != 6 && number != 7)
+                    {
+                        result = "錯誤";
+                    }
+                }
+            
+            ViewBag.Name = name;
+            ViewBag.Result = result;
+
+            return View();
+        }
+    }
+}
